6fe92f818700841231286cd658310c937015804c
hojat72elect <hojat72elect@gmail.com>
2025-11-01 02:04:05 -0400

    small change to stack tests.

--- test/Stack.test.ts
+++ test/Stack.test.ts
@@ -1,71 +1,73 @@
 import {Stack} from "../src/data_structures/Stack";
 
 
-describe('Stack unit tests', () => {
+describe('Stack Data Structure', () => {
+    let stack:Stack;
 
+    beforeEach(() => {
+        stack = new Stack();
+    });
 
     test('Simply pushing and popping a Stack generates LIFO behavior and updates the length of the stack as well.', () => {
 
-        const exampleStack = new Stack();
-        exampleStack.push("h");
-        expect(exampleStack.getSize()).toBe(1);
-        exampleStack.push("o");
-        expect(exampleStack.getSize()).toBe(2);
-        exampleStack.push("j");
-        expect(exampleStack.getSize()).toBe(3);
-        exampleStack.push("a");
-        expect(exampleStack.getSize()).toBe(4);
-        exampleStack.push("t");
-        expect(exampleStack.getSize()).toBe(5);
+        stack.push("h");
+        expect(stack.getSize()).toBe(1);
+        stack.push("o");
+        expect(stack.getSize()).toBe(2);
+        stack.push("j");
+        expect(stack.getSize()).toBe(3);
+        stack.push("a");
+        expect(stack.getSize()).toBe(4);
+        stack.push("t");
+        expect(stack.getSize()).toBe(5);
 
-
-        expect(exampleStack.pop()).toBe("t");
-        expect(exampleStack.getSize()).toBe(4);
-        expect(exampleStack.pop()).toBe("a");
-        expect(exampleStack.getSize()).toBe(3);
-        expect(exampleStack.pop()).toBe("j");
-        expect(exampleStack.getSize()).toBe(2);
-        expect(exampleStack.pop()).toBe("o");
-        expect(exampleStack.getSize()).toBe(1);
-        expect(exampleStack.pop()).toBe("h");
-        expect(exampleStack.getSize()).toBe(0);
+        expect(stack.pop()).toBe("t");
+        expect(stack.getSize()).toBe(4);
+        expect(stack.pop()).toBe("a");
+        expect(stack.getSize()).toBe(3);
+        expect(stack.pop()).toBe("j");
+        expect(stack.getSize()).toBe(2);
+        expect(stack.pop()).toBe("o");
+        expect(stack.getSize()).toBe(1);
+        expect(stack.pop()).toBe("h");
+        expect(stack.getSize()).toBe(0);
 
 
     })
 
     test("Popping an empty Stack should return undefined", () => {
-        const exampleStack = new Stack();
-        expect(exampleStack.pop()).toBeUndefined();
+
+        expect(stack.pop()).toBeUndefined();
     });
 
     test("Peeking into a Stack is safe, doesn't have side effects and wouldn't throw an error for empty Stack", () => {
 
-        const exampleStack = new Stack();
-        expect(exampleStack.peek()).toBeUndefined();
 
-        exampleStack.push(1);
-        expect(exampleStack.peek()).toBe(1);
-        exampleStack.push(2);
-        exampleStack.push(3);
-        expect(exampleStack.peek()).toBe(3);
-        exampleStack.pop();
-        expect(exampleStack.peek()).toBe(2);
+        expect(stack.peek()).toBeUndefined();
+
+        stack.push(1);
+        expect(stack.peek()).toBe(1);
+        stack.push(2);
+        stack.push(3);
+        expect(stack.peek()).toBe(3);
+        stack.pop();
+        expect(stack.peek()).toBe(2);
 
 
     });
 
     test("Clearing a stack will throw away all data but keeps reference to the stack", () => {
 
-        const exampleStack = new Stack();
-        exampleStack.push(12);
-        exampleStack.push(5);
-        exampleStack.push(41);
-        exampleStack.push(86);
-        exampleStack.push(34);
 
-        exampleStack.clear();
-        expect(exampleStack.getSize()).toBe(0);
-        expect(exampleStack.peek()).toBeUndefined();
+        stack.push(12);
+        stack.push(5);
+        stack.push(41);
+        stack.push(86);
+        stack.push(34);
+
+        stack.clear();
+        expect(stack.getSize()).toBe(0);
+        expect(stack.peek()).toBeUndefined();
 
     });
 
